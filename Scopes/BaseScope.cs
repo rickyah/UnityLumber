150a83c9bc6ccae860e45c4455efdc01ba6a1569
Ricardo Amores <ricardo.amores@socialpoint.es>
2014-03-20 14:54:36 +0100

    First commit

---
+++ Scopes/BaseScope.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lumber.Scopes
+{
+    public abstract class BaseScope : IDisposable
+    {
+        protected RichLogger LoggerRef { get; set; }
+
+        internal BaseScope(RichLogger logger)
+        {
+            if (logger == null)
+                throw new ArgumentNullException("logger");
+            
+            LoggerRef = logger;
+        }
+
+        public void Dispose()
+        {
+            this.RestoreState();
+        }
+
+        public abstract void RestoreState();
+    }
+}
---
+++ Scopes/TabScope.cs
@@ -0,0 +1,20 @@
+﻿namespace Lumber.Scopes
+{
+    public class TabScope : BaseScope
+    {
+        private int _oldLevel;
+
+        internal TabScope(RichLogger logger, int newLevel)
+        : base(logger)
+        {
+            _oldLevel = logger.Level;
+        
+            LoggerRef.SetLevel(newLevel);
+        }
+
+        public override void RestoreState()
+        {
+            LoggerRef.SetLevel(_oldLevel);
+        }
+    }
+}
